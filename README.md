d34a7992b196b2e21f24920bd9639f557dd6143b
Robert Repenning <6repenni@informatik.uni-hamburg.de>
2017-11-20 23:30:49 +0100

    updates

---
+++ README.md
@@ -0,0 +1,4 @@
+# Neighborhood Map Project Udacity
+
+#### Sources
+List Design: https://codepen.io/secondfret/pen/mIBqf
---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * hardCoded mapMarkers for POIs
+ * @type {[null,null]}
+ */
+var mapMarkers = [
+    {
+        name: "Bondi Beach",
+        lat: -33.8914755,
+        lon: 151.2766845,
+        street: "Bondi Beach",
+        city: "New South Wales 2026",
+        created: 1851,
+        id: "m0",
+        visible: ko.observable(true)
+    },
+    {
+
+    }
+    ]
